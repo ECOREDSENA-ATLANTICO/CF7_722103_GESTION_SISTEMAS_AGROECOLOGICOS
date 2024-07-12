1050d943e4e9718357ee3edfb17e902fd04ecba1
AndresDiazPinto <andresvis565@gmail.com>
2024-07-12 17:13:58 -0500

    Empaquetado
    Se empaqueta componente, CF finalizado

--- src/config/global.js
+++ src/config/global.js
@@ -13,7 +13,7 @@ export default {
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/images/header/2.svg'),
+        imagen: require('@/assets/curso///images/header/2.svg'),
       },
     ],
   },
